2ca03c10a178c73ae130953f017efbfbcac604d6
Yoon Lee <himynameisyoon@gmail.com>
2019-01-26 09:57:16 -0800

    edit index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import { Provider } from 'react-redux';
+
 import App from './App';
+import store, { history } from './store';
+
+// Styling
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './scss/index.scss';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App history={history} />
+  </Provider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
